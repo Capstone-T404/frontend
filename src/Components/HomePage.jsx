6fc2dce8d1772403b971f0e59b90b444d07ae5ef
Bunji75 <daynelinke@gmail.com>
2025-05-28 11:00:57 +1000

    Change frontend port to 3001 and setup fetching from hosted backend api's

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -22,113 +22,118 @@ const Hero = () => (
     </section>
 );
 
-const EventButton = () => (
-    <section>
-        <button onClick={buttonClicked()}>Click me</button>
-        <button onClick={insertA()}>A</button>
-        <button onClick={insertB()}>B</button>
-        <button onClick={viewData()}>View Data</button>
-        <div id="abc"></div>
-        <button onClick={sendGameEvent()}>Send Game Event</button>
-    </section>
-);
+function EventButton() {
 
-function buttonClicked() {
-    fetch('/buttonClicked', {
-        method: 'POST',
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error');
-            }
-            console.log(response);
-            return response.json();
+    function buttonClicked() {
+        fetch('https://api.bneitconsulting.com/buttonClicked', {
+            method: 'POST',
         })
-        .then(data => {
-            document.getElementById('abc').innerHTML = data.message;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-};
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error');
+                }
+                console.log(response);
+                return response.json();
+            })
+            .then(data => {
+                document.getElementById('abc').innerHTML = data.message;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    };
 
-function insertA() {
-    fetch('/events/insertA', {
-        method: 'POST',
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error');
-            }
-            console.log(response);
-            return response.json();
+    function insertA() {
+        fetch('https://api.bneitconsulting.com/events/insertA', {
+            method: 'POST',
         })
-        .then(data => { })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-function insertB() {
-    fetch('/events/insertB', {
-        method: 'POST',
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error');
-            }
-            console.log(response);
-            return response.json();
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error');
+                }
+                console.log(response);
+                return response.json();
+            })
+            .then(data => { })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+
+    function insertB() {
+        fetch('https://api.bneitconsulting.com/events/insertB', {
+            method: 'POST',
         })
-        .then(data => { })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-function viewData() {
-    fetch('/events/viewData', {
-        method: 'GET',
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error');
-            }
-            console.log(response);
-            return response.json();
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error');
+                }
+                console.log(response);
+                return response.json();
+            })
+            .then(data => { })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+
+    function viewData() {
+        fetch('https://api.bneitconsulting.com/events/viewData', {
+            method: 'GET',
         })
-        .then(data => {
-            document.getElementById('abc').innerHTML = data.message;
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error');
+                }
+                console.log(response);
+                return response.json();
+            })
+            .then(data => {
+                document.getElementById('abc').innerHTML = data.message;
+            })
+            .catch(error => {
+                console.error('Error :', error);
+            });
+    }
+
+    function sendGameEvent() {
+        fetch('https://api.bneitconsulting.com/events/newGameEvent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                time: 1,
+                event: 'R',
+                zone: 'A',
+            }),
         })
-        .catch(error => {
-            console.error('Error :', error);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error');
+                }
+                console.log(response);
+                return response.json();
+            })
+            // .then(data => {
+            //     console.log('data: ', data.message.gameTime);
+            //     (document.getElementById('abc').innerHTML = data.message.gameTime),
+            //         data.message.gameEvent,
+            //         data.message.gameZone;
+            // })
+            .catch(error => {
+                console.error('Error :', error);
+            });
+    }
+    return (
+        <section>
+            <button onClick={buttonClicked}>Click me</button>
+            <button onClick={insertA}>A</button>
+            <button onClick={insertB}>B</button>
+            <button onClick={viewData}>View Data</button>
+            <div id="abc"></div>
+            <button onClick={sendGameEvent}>Send Game Event</button>
+        </section >
+    );
 }
 
-function sendGameEvent() {
-    fetch('/newGameEvent', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            time: 1,
-            event: 'R',
-            zone: 'A',
-        }),
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error');
-            }
-            console.log(response);
-            return response.json();
-        })
-        // .then(data => {
-        //     console.log('data: ', data.message.gameTime);
-        //     (document.getElementById('abc').innerHTML = data.message.gameTime),
-        //         data.message.gameEvent,
-        //         data.message.gameZone;
-        // })
-        .catch(error => {
-            console.error('Error :', error);
-        });
-}
